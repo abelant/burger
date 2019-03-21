ac49cd990d8a306344de590c1cd11bccfcf0f31d
Abelant Zairi <abelant@outlook.com>
2019-03-21 17:47:14 +0100

    Adding Navigation-Menu,Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliry from '../../hoc/Auxiliry';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxiliry>
-        <Toolbar />       
-        <main className={classes.Content}>
-            {props.children}
-        </main>   
-    </Auxiliry>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () =>{
+        this.setState({showSideDrawer:false});
+    }
+    sideDrawerToggleHandler = () =>{
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+    }
 
-export default layout;
+    render(){
+        return(
+        <Auxiliry>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>    
+            <SideDrawer 
+                closed={this.sideDrawerClosedHandler}  
+                open={this.state.showSideDrawer}/>  
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>   
+        </Auxiliry>
+        )
+    }
+}
+
+export default Layout;
